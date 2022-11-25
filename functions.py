442eb60d9f95210731c5f17207855fc48af465b3
JG-11 <jg08950@gmail.com>
2022-11-25 16:54:24 -0600

    Helper functions

---
+++ functions.py
@@ -0,0 +1,17 @@
+import datetime
+
+def serialize_sensor_data(data):
+    sensor_data = []
+
+    for record in data:
+        sensor_data.append({
+            'co2': record.co2,
+            'co2_status': record.co2_status,
+            'temperature': record.temperature,
+            'temperature_status': record.temperature_status,
+            'humidity': record.humidity,
+            'humidity_status': record.humidity_status,
+            'created_at': datetime.strptime(record.created_at, '%Y-%m-%d %H:%M'),
+        })
+
+    return sensor_data
